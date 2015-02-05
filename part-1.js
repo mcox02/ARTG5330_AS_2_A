c1b36b6ba656fd39544817b5c83a5a4622697fc5
mcox02 <matthew.ford.cox@gmail.com>
2015-02-05 12:07:20 -0500

    Part 1 done on Feb 5

--- part-1.js
+++ part-1.js
@@ -2,8 +2,6 @@
 //Due Feb 12
 //This short exercise will help you gain additional proficiency working with JavaScript objects
 
-
-//Information about cities can be easily represented as JavaScript objects
 var city1 = {
 		name:"Boston",
 		pop:657000,
@@ -18,15 +16,26 @@ var city2 = {
 //TODO: write a function that takes two city objects as parameter
 //and returns the name of city that has a larger population
 function whichCityIsLarger(a,b){
-	//hint: since a and b are objects, you can access their "pop" property using a.pop
-	//compare populations, and return the "name" property of the appropriate city
-
+	if (a.pop > b.pop) {
+		return a.name + " has a greater population.";
+	} else{
+		if(a.pop < b.pop){
+			return b.name + " has a greater population.";
+		}
+	}
 }
 console.log( whichCityIsLarger(city1,city2) ); 
 
 
 //TODO: write a function that returns the name of the city that is older
 function whichCityIsOlder(a,b){
-	//hint: more or less the same as above, but notice something weird about the property name "year founded"?
-	//Can you still use the dot notation? If not, what should you do?
+
+	if (a["year founded"] < b["year founded"]) {
+		return a.name + " is the older city.";
+	} else{
+		if(a["year founded"] > b["year founded"]){
+			return b.name + " is the older city.";
+		}
+	}
 }
+console.log( whichCityIsOlder(city1,city2) );
