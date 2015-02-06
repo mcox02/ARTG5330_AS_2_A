ed18e57b3d0d6bf2c474574bd5f437e5791ccdb7
mcox02 <matthew.ford.cox@gmail.com>
2015-02-06 17:26:26 -0500

    Part 2 of Assignment 2 done on Feb 06

--- part-2.js
+++ part-2.js
@@ -6,10 +6,10 @@
 //This creates an empty new array
 var newArray = [];
 
-//TODO: populate the new array with 1000 new random values between 50 and 200
-//hint: array.push
-//hint: Math.random() creates a random value between 0 and 1
-
+for (var i = 0; i <1000; i++) {
+	var dataPoint = (Math.random() * 150) + 50;
+	newArray.push (dataPoint);
+};
 
 //TODO: after you populate the array, find the average of all the values in the array
 
@@ -18,4 +18,13 @@ newArray.forEach(function(arrayElement){
 	//hint: here, we are "looping" through the array, accessing each array element one by one
 	//we can add the value to arraySum
 	//and finally, we divide arraySum by the total number of array elements
+			arraySum = arraySum + arrayElement;
+	
+arrayAverage = arraySum / newArray.length;
+
 });
+console.log(arraySum);
+console.log(arrayAverage);
+
+console.log("This data set of " + newArray.length + " items has a sum of " + arraySum + 
+		" and an average of " + arrayAverage + ".");
